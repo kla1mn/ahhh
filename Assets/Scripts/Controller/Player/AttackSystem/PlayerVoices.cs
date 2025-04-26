e36d5925b9f7b8860878521b01a764fa1c900562
Михаил Владимиров <mikhail.vladimirov@urfu.me>
2025-04-27 04:37:25 +0500

    Add pain voices

--- Assets/Scripts/Controller/Player/AttackSystem/PlayerVoices.cs
+++ Assets/Scripts/Controller/Player/AttackSystem/PlayerVoices.cs
@@ -4,7 +4,8 @@ public class PlayerVoices : MonoBehaviour
 {
     [SerializeField] private PlayerState playerState;
     [SerializeField] private AudioSource audioSource;
-    [SerializeField] private AudioClip[] audioClips;
+    [SerializeField] private AudioClip[] attackSounds;
+    [SerializeField] private AudioClip[] painSounds;
     
     void Update()
     {
@@ -12,21 +13,36 @@ public class PlayerVoices : MonoBehaviour
         {
             PlayRandomAttackSound();
         }
+
+        if (playerState.IsHearting)
+        {
+            PlayRandomPainEffect();
+        }
     }
     
     private void PlayRandomAttackSound()
+    {
+        PlayRandomEffect(attackSounds);
+    }
+    
+    private void PlayRandomPainEffect()
+    {
+        PlayRandomEffect(painSounds);
+    }
+
+    private void PlayRandomEffect(AudioClip[] clips)
     {
         const float playChance = 1 / 3f;
         var randomValue = Random.value;
 
         if (true)// if (randomValue < playChance)
         {
-            if (audioClips != null && audioClips.Length > 0)
+            if (clips != null && clips.Length > 0)
             {
                 if (!audioSource.isPlaying) // Проверка
                 {
-                    var randomIndex = UnityEngine.Random.Range(0, audioClips.Length);
-                    var randomClip = audioClips[randomIndex];
+                    var randomIndex = UnityEngine.Random.Range(0, clips.Length);
+                    var randomClip = clips[randomIndex];
                     audioSource.PlayOneShot(randomClip);
                 }
             }
