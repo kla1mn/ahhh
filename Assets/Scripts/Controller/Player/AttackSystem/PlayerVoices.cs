ced80ff855a682fba6a8860bb1eee740ffafe1e9
Михаил Владимиров <mikhail.vladimirov@urfu.me>
2025-04-27 05:32:45 +0500

    Add jump

--- Assets/Scripts/Controller/Player/AttackSystem/PlayerVoices.cs
+++ Assets/Scripts/Controller/Player/AttackSystem/PlayerVoices.cs
@@ -6,43 +6,81 @@ public class PlayerVoices : MonoBehaviour
     [SerializeField] private AudioSource audioSource;
     [SerializeField] private AudioClip[] attackSounds;
     [SerializeField] private AudioClip[] painSounds;
-    
+    [SerializeField] private AudioClip[] jumpSounds;
+
+    private bool wasAttacking = false;
+    private bool wasHearting = false;
+    private bool wasJumping = false;
+
     void Update()
     {
         if (playerState.IsAttacking)
         {
-            PlayRandomAttackSound();
+            if (!wasAttacking)
+            {
+                wasAttacking = true;
+                PlayRandomAttackSound();
+            }
+        }
+        else
+        {
+            wasAttacking = false;
         }
 
         if (playerState.IsHearting)
         {
-            PlayRandomPainEffect();
+            if (!wasHearting)
+            {
+                wasHearting = true;
+                PlayRandomPainEffect();
+            }
+        }
+        else
+        {
+            wasHearting = false;
+        }
+
+        if (playerState.IsJumping)
+        {
+            if (!wasJumping)
+            {
+                wasJumping = true;
+                PlayRandomJumpEffect();
+            }
+        }
+        else
+        {
+            wasJumping = false;
         }
     }
-    
+
     private void PlayRandomAttackSound()
     {
         PlayRandomEffect(attackSounds);
     }
-    
+
     private void PlayRandomPainEffect()
     {
         PlayRandomEffect(painSounds);
     }
 
+    private void PlayRandomJumpEffect()
+    {
+        PlayRandomEffect(jumpSounds);
+    }
+
     private void PlayRandomEffect(AudioClip[] clips)
     {
-        const float playChance = 1 / 3f;
-        var randomValue = Random.value;
+        int randomValue = UnityEngine.Random.Range(0, 100); // Случайное число от 0 до 99
 
-        if (true)// if (randomValue < playChance)
+        if (randomValue < 33) // 33% шанс
         {
-            if (clips != null && clips.Length > 0)
+            if (clips != null && clips.Length > 0 && audioSource != null)
             {
-                if (!audioSource.isPlaying) // Проверка
+                if (!audioSource.isPlaying) // Проверка чтобы не наслаивать звуки
                 {
-                    var randomIndex = UnityEngine.Random.Range(0, clips.Length);
-                    var randomClip = clips[randomIndex];
+                    int randomIndex = UnityEngine.Random.Range(0, clips.Length);
+                    AudioClip randomClip = clips[randomIndex];
                     audioSource.PlayOneShot(randomClip);
                 }
             }
