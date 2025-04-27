c08fb7390ce0aef71bd67299f0f80ecb354837b1
IVAN <86919046+Yackunia@users.noreply.github.com>
2025-04-27 08:03:18 +0500

    fix

--- Assets/Scripts/EnemySystem/BossTrigger.cs
+++ Assets/Scripts/EnemySystem/BossTrigger.cs
@@ -6,22 +6,16 @@ public class BossTrigger : MonoBehaviour
 {
     [Header("Boss Settings")]
     [SerializeField] private GroundEnemy bossEnemy;
+    [SerializeField] private FlyingEnemy bossFlyEnemy;
     [SerializeField] private GameObject flexObjts;
 
-    private void Awake()
-    {
-
-        if (bossEnemy != null)
-        {
-            bossEnemy.SetActive(false);
-        }
-    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("PlayerRb"))
         {
             if (bossEnemy != null) bossEnemy.SetActive(true);
+            if (bossFlyEnemy != null) bossFlyEnemy.SetActive(true);
 
             SwapVolume(true);
         }
